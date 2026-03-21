6738b419164995b0221b9d6c7de8e375ec1c49dc
ynzung <syj91105@sookmyung.ac.kr>
2026-03-21 14:13:08 +0900

    setting: vanilla-extract plugin을 next.config에 적용(#5)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 import type { NextConfig } from 'next';
+
+const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
