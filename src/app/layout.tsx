df5755a9624e0572c2844cc8e4798a2e097fff49
ynzung <syj91105@sookmyung.ac.kr>
2026-04-03 19:22:16 +0900

    refactor: fonts 디렉토리를 src에서 public으로 이동(#12)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from 'next/font/local';
 import '@/shared/styles';
 
 const pretendard = localFont({
-  src: '../fonts/PretendardVariable.woff2',
+  src: '../../public/fonts/PretendardVariable.woff2',
   display: 'swap',
   weight: '100 900',
   variable: '--font-pretendard',
