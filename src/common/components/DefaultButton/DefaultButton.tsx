e27020ea38814e167b3fe5547c297bb186f0f14e
solux <lee.yeji0327@gmail.com>
2026-04-07 03:06:41 +0900

    chore: button type prop 추가 및 명시(#19)

--- src/common/components/DefaultButton/DefaultButton.tsx
+++ src/common/components/DefaultButton/DefaultButton.tsx
@@ -6,11 +6,17 @@ interface DefaultButtonPropTypes {
   label: string;
   onClick?: () => void;
   disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-const DefaultButton = ({ label, onClick, disabled = false }: DefaultButtonPropTypes) => {
+const DefaultButton = ({
+  label,
+  onClick,
+  disabled = false,
+  type = 'button',
+}: DefaultButtonPropTypes) => {
   return (
-    <button className={defaultButton} onClick={onClick} disabled={disabled}>
+    <button className={defaultButton} onClick={onClick} disabled={disabled} type={type}>
       {label}
     </button>
   );
