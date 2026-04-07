7fc1da315c1fc65d7b87201a9b7f20d3cdac41a4
solux <lee.yeji0327@gmail.com>
2026-04-07 13:21:13 +0900

    feat: layout에 header 적용(#21)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import '@/shared/styles';
+import Header from '@/common/components/Header/Header';
 
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={pretendard.variable}>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
