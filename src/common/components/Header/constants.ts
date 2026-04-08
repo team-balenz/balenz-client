4ebc19192d5c73aa31d7672db8cccede0aebbea9
solux <lee.yeji0327@gmail.com>
2026-04-09 03:04:22 +0900

    comment: header 관련 상수 주석 추가(#21)

--- src/common/components/Header/constants.ts
+++ src/common/components/Header/constants.ts
@@ -1,10 +1,21 @@
+/**
+ * 헤더의 네비게이션 메뉴 항목들
+ * 각 항목은 라벨(화면에 표시되는 텍스트)과 경로(href)로 구성
+ */
 export const NAV_ITEMS = [
   { label: '이념 관점', href: '/' },
   { label: '글로벌 관점', href: '/global' },
 ];
 
+/**
+ * 기본 활성 네비게이션 항목
+ * 페이지 로드 시 처음 표시될 활성 탭을 지정
+ */
 export const DEFAULT_ACTIVE_TAB = NAV_ITEMS[0].label;
 
+/**
+ * 로고 이미지의 반응형 크기 (width, height, aspectRatio)
+ */
 export const LOGO_SIZE = {
   desktop: {
     width: '167px',
@@ -23,6 +34,9 @@ export const LOGO_SIZE = {
   },
 };
 
+/**
+ * 헤더의 반응형 레이아웃 설정 (padding, height)
+ */
 export const HEADER_LAYOUT = {
   desktop: {
     padding: '0 80px',
@@ -38,6 +52,11 @@ export const HEADER_LAYOUT = {
   },
 };
 
+/**
+ * 헤더에서 사용되는 아이콘들의 경로
+ * - menu: 모바일 메뉴 버튼
+ * - search: 검색 버튼
+ */
 export const ICONS = {
   menu: '/icons/ic_menu.svg',
   search: '/icons/ic_search.svg',
