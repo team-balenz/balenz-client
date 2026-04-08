34f32e15690d8c91bd5bc808bb8ac8a1a285d016
solux <lee.yeji0327@gmail.com>
2026-04-09 03:19:43 +0900

    comment: route 관련 주석 수정(#21)

--- src/shared/constants/route.ts
+++ src/shared/constants/route.ts
@@ -1,7 +1,5 @@
 /**
  * 애플리케이션 내 모든 라우트 경로를 정의하는 상수
- * 네비게이션, 리다이렉트, 링크 생성 등에 사용되며,
- * 경로가 변경될 때 이 파일만 수정하면 된다.
  *
  * 사용 예시:
  * - Link의 href: `<Link href={ROUTES.IDEOLOGY}>이념 관점</Link>`
@@ -18,7 +16,7 @@ export const ROUTES = {
 
 /**
  * ROUTES의 값 중 하나가 될 수 있는 라우트 경로 타입
- * TypeScript 타입 안전성을 제공하여 잘못된 경로 값을 방지한다.
+ * TypeScript 타입 안전성을 제공하여 잘못된 경로 값을 방지
  *
  * 사용 예시:
  * - 함수 파라미터: `function navigate(path: RoutePath) { ... }`
