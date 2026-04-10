baa1326e05b324438097274780b183ea6da026ac
ynzung <syj91105@sookmyung.ac.kr>
2026-04-10 15:34:50 +0900

    feat: HotIssueSection 목데이터 생성(#29)

---
+++ src/mocks/data/home.ts
@@ -0,0 +1,142 @@
+import { type HotIssueGroupTypes } from '@/app/(home)/types/hotIssueSection';
+
+export const HOT_ISSUE_ARTICLE_GROUP: HotIssueGroupTypes[] = [
+  {
+    keywordId: 1,
+    ideology: 'progressive',
+    keyword: '키워드',
+    articleItems: [
+      {
+        articleId: 1,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SL',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+      {
+        articleId: 2,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'L',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+    ],
+  },
+  {
+    keywordId: 2,
+    ideology: 'conservative',
+    keyword: '키워드',
+    articleItems: [
+      {
+        articleId: 3,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SR',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+      {
+        articleId: 4,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SR',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+    ],
+  },
+  {
+    keywordId: 3,
+    ideology: 'progressive',
+    keyword: '키워드',
+    articleItems: [
+      {
+        articleId: 5,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SL',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+      {
+        articleId: 6,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'L',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+    ],
+  },
+  {
+    keywordId: 4,
+    ideology: 'conservative',
+    keyword: '키워드',
+    articleItems: [
+      {
+        articleId: 7,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SR',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+      {
+        articleId: 8,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SR',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+    ],
+  },
+  {
+    keywordId: 5,
+    ideology: 'progressive',
+    keyword: '키워드',
+    articleItems: [
+      {
+        articleId: 9,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SL',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+      {
+        articleId: 10,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'L',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+    ],
+  },
+  {
+    keywordId: 6,
+    ideology: 'conservative',
+    keyword: '키워드',
+    articleItems: [
+      {
+        articleId: 11,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SR',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+      {
+        articleId: 12,
+        articleThumbnail: 'https://i.pinimg.com/736x/44/b0/f0/44b0f0315ece65cdc3b64130c91ea009.jpg',
+        ideologyIndicatorValue: 'SR',
+        articleTitle:
+          '해당 섹션은 기사의 제목을 작성하는 섹션입니다. 세 줄 초과의 경우, 말줄임표 적용해주세요.',
+        mediaName: '언론사명',
+      },
+    ],
+  },
+];
