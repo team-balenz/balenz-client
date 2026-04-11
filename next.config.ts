f7d8f9c9b765a38febe77aa47c253a3d676be7b2
solux <lee.yeji0327@gmail.com>
2026-04-12 05:21:06 +0900

    chore: 외부 이미지 도메인 임시 허용 설정(#33)

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,18 @@ import type { NextConfig } from 'next';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default withVanillaExtract(nextConfig);
