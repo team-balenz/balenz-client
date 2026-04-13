a139d22cf195888163dd9ea5a2bbbcd6dfc3cf44
solux <lee.yeji0327@gmail.com>
2026-04-13 21:42:11 +0900

    chore: use client 제거(#42)

--- src/shared/components/scopeArticleItem/ScopeArticleItem.tsx
+++ src/shared/components/scopeArticleItem/ScopeArticleItem.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from 'next/image';
 import ScopePercentBar from '@/common/components/percentBar/ScopePercentBar';
 import { IDEOLOGY_LABELS, type IdeologyType } from './constants';
