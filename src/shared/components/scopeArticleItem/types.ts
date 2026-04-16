0e6b18542adfe14c273ac8e0c3523af7b054f4d2
solux <lee.yeji0327@gmail.com>
2026-04-17 01:48:13 +0900

    comment: JSDoc 속성명 오류 수정(#46)

--- src/shared/components/scopeArticleItem/types.ts
+++ src/shared/components/scopeArticleItem/types.ts
@@ -5,7 +5,7 @@ export type IdeologyType = 'progressive' | 'center' | 'conservative';
 
 /**
  * ScopeArticleItem 컴포넌트에 전달되는 기사 아이템 데이터
- * @property id - 고유 식별자
+ * @property keywordId - 키워드 고유 식별자
  * @property imageUrl - 기사 이미지 URL
  * @property imageAlt - 이미지 대체 텍스트
  * @property keyword - 키워드 (주제)
