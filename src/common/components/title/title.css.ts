1ad22ac22a60adc1d0f58407a799ac24ce731970
solux <lee.yeji0327@gmail.com>
2026-04-30 01:50:12 +0900

    feat: Title 컴포넌트 variant별 스타일 추가(#70)

--- src/common/components/title/title.css.ts
+++ src/common/components/title/title.css.ts
@@ -1,31 +1,114 @@
-import { style } from '@vanilla-extract/css';
 import { media } from '@/shared/styles/media';
 import { typography, color } from '@/shared/styles';
+import { recipe } from '@vanilla-extract/recipes';
 
-export const subtitleStyle = style({
-  ...typography.correction,
-  ...typography.desktop.h3,
-  color: color.text.tertiary,
-  '@media': {
-    [media.tablet]: {
-      ...typography.tablet.h3,
+/**
+ * Title 컴포넌트 컨테이너 스타일
+ * articleHeader만 gap 존재
+ */
+export const container = recipe({
+  base: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  variants: {
+    variant: {
+      default: {
+        gap: 0,
+      },
+      articleHeader: {
+        gap: '0.625rem',
+      },
+      articleSummary: {},
     },
-    [media.mobile]: {
-      ...typography.phone.h3,
-    },
+  },
+  defaultVariants: {
+    variant: 'default',
   },
 });
 
-export const titleStyle = style({
-  ...typography.correction,
-  ...typography.desktop.h1,
-  color: color.text.main,
-  '@media': {
-    [media.tablet]: {
-      ...typography.tablet.h1,
+/**
+ * 부제목 스타일
+ * articleSummary만 타이포 상이
+ */
+export const subtitleStyle = recipe({
+  base: {
+    ...typography.correction,
+    ...typography.desktop.h3,
+    color: color.text.tertiary,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    '@media': {
+      [media.tablet]: {
+        ...typography.tablet.h3,
+      },
+      [media.mobile]: {
+        ...typography.phone.h3,
+      },
     },
-    [media.mobile]: {
-      ...typography.phone.h1,
+  },
+  variants: {
+    variant: {
+      default: {},
+      articleHeader: {},
+      articleSummary: {
+        ...typography.desktop.h4,
+        '@media': {
+          [media.tablet]: {
+            ...typography.tablet.body1,
+          },
+          [media.mobile]: {
+            ...typography.phone.body2,
+          },
+        },
+      },
     },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+/**
+ * 메인 제목 스타일
+ * articleHeader만 타이포 상이
+ */
+export const titleStyle = recipe({
+  base: {
+    ...typography.correction,
+    ...typography.desktop.h1,
+    color: color.text.main,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    '@media': {
+      [media.tablet]: {
+        ...typography.tablet.h1,
+      },
+      [media.mobile]: {
+        ...typography.phone.h1,
+      },
+    },
+  },
+  variants: {
+    variant: {
+      default: {},
+      articleHeader: {
+        ...typography.desktop.display,
+        '@media': {
+          [media.tablet]: {
+            ...typography.tablet.display,
+          },
+          [media.mobile]: {
+            ...typography.phone.display,
+          },
+        },
+      },
+      articleSummary: {},
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
   },
 });
