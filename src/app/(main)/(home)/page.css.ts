0db141451b482be5054e384a7b41d6de0a173d42
solux <lee.yeji0327@gmail.com>
2026-05-18 21:39:35 +0900

    feat: 메인 이념페이지 레이아웃 완성(#105)

---
+++ src/app/(main)/(home)/page.css.ts
@@ -0,0 +1,156 @@
+import { boxStyle, color, media } from '@/shared/styles';
+import { style } from '@vanilla-extract/css';
+
+export const homeLayout = style({
+  width: '100%',
+  padding: '5rem 5rem 14.125rem 5rem',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '4.5625rem',
+
+  '@media': {
+    [media.tablet]: {
+      padding: '2.5rem 2.5rem 0 2.5rem',
+      gap: '8.125rem',
+    },
+    [media.mobile]: {
+      padding: '1.5625rem 1.25rem 0 1.25rem',
+      gap: '6.5rem',
+    },
+  },
+});
+
+export const topSection = style({
+  width: '100%',
+  display: 'grid',
+  gridTemplateColumns: '66.666% 33.333%',
+  gridTemplateAreas: `
+    "scope right"
+    "adDaily right"
+    "hot right"
+  `,
+  columnGap: '1.5rem',
+  rowGap: '6.25rem',
+
+  '@media': {
+    [media.tablet]: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '6.25rem',
+      gridTemplateAreas: 'unset',
+    },
+    [media.mobile]: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '5rem',
+      gridTemplateAreas: 'unset',
+    },
+  },
+});
+
+export const scopeSection = style({
+  gridArea: 'scope',
+});
+
+export const adDailyColumn = style({
+  gridArea: 'adDaily',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.875rem',
+
+  '@media': {
+    [media.tablet]: {
+      gap: '5.4375rem',
+    },
+  },
+});
+
+export const ad1Section = style({
+  height: '7.375rem',
+  background: color.brand.gray1,
+  borderRadius: '5px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '@media': {
+    [media.mobile]: {
+      display: 'none',
+    },
+  },
+});
+
+export const rightColumn = style({
+  width: '100%',
+  gridArea: 'right',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.5rem',
+
+  '@media': {
+    [media.tablet]: {
+      flexDirection: 'row',
+      gap: '1.5625rem',
+    },
+    [media.mobile]: {
+      gap: '5rem',
+    },
+  },
+});
+
+export const ad2Section = style({
+  ...boxStyle,
+  height: '16.25rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '@media': {
+    [media.tablet]: {
+      display: 'none',
+    },
+    [media.mobile]: {
+      display: 'none',
+    },
+  },
+});
+
+export const dailyFocusSection = style({});
+
+export const hotIssueSection = style({
+  gridArea: 'hot',
+});
+
+export const ideologyTestCTA = style({
+  width: '100%',
+  height: '9.9375rem',
+  ...boxStyle,
+
+  '@media': {
+    [media.tablet]: {
+      width: '100%',
+      height: '15.375rem',
+    },
+    [media.mobile]: {
+      width: '100%',
+      height: '9.6875rem',
+    },
+  },
+});
+
+export const hotArticleSection = style({});
+
+export const bottomSection = style({
+  padding: '6.25rem 5rem',
+  background: color.brand.gray1,
+  marginLeft: '-5rem',
+  width: 'calc(100% + 10rem)',
+
+  '@media': {
+    [media.mobile]: {
+      padding: '3.125rem 1.25rem',
+      marginLeft: '-1.25rem',
+      width: 'calc(100% + 3rem)',
+    },
+  },
+});
--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -1,3 +1,40 @@
+'use client';
+
+import ByIdeologySection from './components/ByIdeologySection/ByIdeologySection';
+import DailyFocusSection from './components/dailyFocusSection/DailyFocusSection';
+import HotArticleSection from './components/hotArticleSection/HotArticleSection';
+import HotIssueSection from './components/hotIssueSection/HotIssueSection';
+import ScopeSection from './components/scopeSection/ScopeSection';
+import * as styles from './page.css';
+
 export default function Home() {
-  return <p>balenz</p>;
+  return (
+    <div className={styles.homeLayout}>
+      <div className={styles.topSection}>
+        <div className={styles.scopeSection}>
+          <ScopeSection />
+        </div>
+        <div className={styles.adDailyColumn}>
+          <div className={styles.ad1Section}>ad</div>
+          <div className={styles.dailyFocusSection}>
+            <DailyFocusSection />
+          </div>
+        </div>
+        <div className={styles.rightColumn}>
+          <div className={styles.ideologyTestCTA}></div>
+          <div className={styles.ad2Section}>ad</div>
+          <div className={styles.hotArticleSection}>
+            <HotArticleSection />
+          </div>
+          <div className={styles.ad2Section}>ad</div>
+        </div>
+        <div className={styles.hotIssueSection}>
+          <HotIssueSection />
+        </div>
+      </div>
+      <div className={styles.bottomSection}>
+        <ByIdeologySection />
+      </div>
+    </div>
+  );
 }
