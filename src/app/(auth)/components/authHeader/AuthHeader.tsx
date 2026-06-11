6663e497823135c4675452d62a58d9810818fee4
ynzung <syj91105@sookmyung.ac.kr>
2026-06-12 00:30:26 +0900

    feat: AuthHeader 컴포넌트 구현(#134)

---
+++ src/app/(auth)/components/authHeader/AuthHeader.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+import * as styles from './authHeader.css';
+
+interface AuthHeaderProps {
+  title: string;
+  description: string;
+}
+
+const AuthHeader = ({ title, description }: AuthHeaderProps) => {
+  return (
+    <div className={styles.container}>
+      <Image
+        src="/logo/auth_logo.svg"
+        alt="Balenz 로고"
+        className={styles.logoImg}
+        width={50}
+        height={60}
+      />
+
+      <h1 className={styles.title}>{title}</h1>
+
+      {description && <p className={styles.description}>{description}</p>}
+    </div>
+  );
+};
+
+export default AuthHeader;
