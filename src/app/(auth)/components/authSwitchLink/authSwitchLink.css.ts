7c389ff3781a281adc46b35ce2917625d7c0a295
ynzung <syj91105@sookmyung.ac.kr>
2026-06-12 00:58:04 +0900

    feat: AuthSwitchLink 컴포넌트 구현(#136)

---
+++ src/app/(auth)/components/authSwitchLink/authSwitchLink.css.ts
@@ -0,0 +1,36 @@
+import { style } from '@vanilla-extract/css';
+
+import { color, typography, media } from '@/shared/styles';
+
+export const container = style({
+  ...typography.correction,
+  ...typography.desktop.body3,
+  color: color.text.disabled,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '0.4rem',
+  '@media': {
+    [media.tablet]: {
+      ...typography.tablet.body3,
+    },
+    [media.mobile]: {
+      ...typography.phone.body3,
+    },
+  },
+});
+
+export const link = style({
+  ...typography.correction,
+  ...typography.desktop.body3,
+  color: color.text.main,
+  textDecoration: 'underline',
+  '@media': {
+    [media.tablet]: {
+      ...typography.tablet.button,
+    },
+    [media.mobile]: {
+      ...typography.phone.button,
+    },
+  },
+});
