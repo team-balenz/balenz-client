6842515e40c4437df7a4bfabd3352e6f32bec7c8
solux <lee.yeji0327@gmail.com>
2026-06-15 23:08:41 +0900

    feat: search page 컴포넌트 조립 및 레이아웃 완성(#151)

---
+++ src/app/(main)/search/page.css.ts
@@ -0,0 +1,143 @@
+import { style } from '@vanilla-extract/css';
+import { color, media, typography } from '@/shared/styles';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '@media': {
+    [media.mobile]: {
+      padding: '1.25rem',
+    },
+  },
+});
+
+export const contentWrapper = style({
+  width: '39.25rem',
+  marginTop: '5.81rem',
+  marginBottom: '2.56rem',
+
+  '@media': {
+    [media.tablet]: {
+      width: '31.875rem',
+      marginTop: '5.94rem',
+      marginBottom: '6.87rem',
+    },
+    [media.mobile]: {
+      width: '100%',
+      marginTop: '3.13rem',
+      marginBottom: '1.75rem',
+    },
+  },
+});
+
+export const tabSection = style({
+  width: '100%',
+  borderBottom: `0.5px solid ${color.brand.gray2}`,
+  marginTop: '2.62rem',
+
+  '@media': {
+    [media.tablet]: {
+      marginTop: '3.12rem',
+    },
+    [media.mobile]: {
+      marginTop: '2.5rem',
+    },
+  },
+});
+
+export const searchInputWrapper = style({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.25rem',
+});
+
+export const inputTitle = style({
+  ...typography.desktop.h1,
+
+  '@media': {
+    [media.tablet]: {
+      ...typography.tablet.h1,
+    },
+    [media.mobile]: {
+      ...typography.phone.h1,
+    },
+  },
+});
+
+export const tabWrapper = style({
+  width: '15rem',
+
+  '@media': {
+    [media.mobile]: {
+      width: '12.5rem',
+    },
+  },
+});
+
+export const resultCount = style({
+  ...typography.desktop.body1,
+  marginTop: '2.19rem',
+
+  '@media': {
+    [media.tablet]: {
+      ...typography.tablet.body1,
+      marginTop: '1.87rem',
+    },
+    [media.mobile]: {
+      ...typography.phone.body1,
+      marginTop: '1.25rem',
+    },
+  },
+});
+
+export const resultListWrapper = style({
+  marginTop: '1rem',
+
+  '@media': {
+    [media.tablet]: {
+      marginTop: '0.63rem',
+    },
+    [media.mobile]: {
+      marginTop: '0.62rem',
+    },
+  },
+});
+
+export const pagenation = style({
+  margin: 'auto',
+  width: '19.875rem',
+  marginTop: '3.37rem',
+
+  '@media': {
+    [media.tablet]: {
+      marginTop: '2.5rem',
+    },
+    [media.mobile]: {
+      marginTop: '1.88rem',
+    },
+  },
+});
+
+export const adSection = style({
+  width: '100%',
+  height: '8.125rem',
+  background: color.brand.gray1,
+  borderRadius: '0.3125rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginTop: '8.56rem',
+
+  '@media': {
+    [media.tablet]: {
+      marginTop: '12rem',
+    },
+    [media.mobile]: {
+      marginTop: '12.75rem',
+    },
+  },
+});
