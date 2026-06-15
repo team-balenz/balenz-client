e03813555248cd7d38f94dca0b28be51b2d82353
solux <lee.yeji0327@gmail.com>
2026-06-15 22:44:51 +0900

    fix: tabsContainer div 오류 수정(#143)

--- src/app/(main)/search/components/searchResultContent/newsResultList/NewsResultList.tsx
+++ src/app/(main)/search/components/searchResultContent/newsResultList/NewsResultList.tsx
@@ -12,13 +12,14 @@ interface NewsListPropTypes {
 const NewsList = ({ items }: NewsListPropTypes) => {
   return (
     <section className={styles.section}>
-      <div className={styles.tabsContainer}></div>
-      <div className={styles.articlesContainer}>
-        {items.map((article) => (
-          <div key={article.articleId} className={styles.articleItem}>
-            <ArticlePreviewItem {...article} />
-          </div>
-        ))}
+      <div className={styles.tabsContainer}>
+        <div className={styles.articlesContainer}>
+          {items.map((article) => (
+            <div key={article.articleId} className={styles.articleItem}>
+              <ArticlePreviewItem {...article} />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
