cf8339f4ae56bb3fe51f0972fca107a4abeee613
ynzung <syj91105@sookmyung.ac.kr>
2026-06-16 00:10:57 +0900

    style: 로그인 폼 반응형 gap 및 버튼 여백 추가(#140)

--- src/app/(auth)/login/components/loginForm/loginForm.css.ts
+++ src/app/(auth)/login/components/loginForm/loginForm.css.ts
@@ -1,4 +1,5 @@
 import { style } from '@vanilla-extract/css';
+import { media } from '@/shared/styles';
 
 export const form = style({
   display: 'flex',
@@ -9,8 +10,24 @@ export const inputGroup = style({
   display: 'flex',
   flexDirection: 'column',
   gap: '0.12rem',
+  '@media': {
+    [media.tablet]: {
+      gap: '0.44rem',
+    },
+    [media.mobile]: {
+      gap: '0.38rem',
+    },
+  },
 });
 
 export const buttonWrapper = style({
-  marginTop: '3.44rem',
+  marginTop: '2.19rem',
+  '@media': {
+    [media.tablet]: {
+      marginTop: '2.38rem',
+    },
+    [media.mobile]: {
+      marginTop: '2.31rem',
+    },
+  },
 });
