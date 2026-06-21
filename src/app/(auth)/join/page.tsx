c665584e3ed2d9d4952f65aa5d44f3477550c5d2
ynzung <syj91105@sookmyung.ac.kr>
2026-06-21 15:27:43 +0900

    feat: 회원가입 페이지 컴포넌트 조립 및 레이아웃 구현(#168)

--- src/app/(auth)/join/page.tsx
+++ src/app/(auth)/join/page.tsx
@@ -1,3 +1,20 @@
+import AuthHeader from '../components/authHeader/AuthHeader';
+import AuthSwitchLink from '../components/authSwitchLink/AuthSwitchLink';
+import SocialLoginSection from '../components/socialLoginSection/SocialLoginSection';
+import JoinForm from './components/joinForm/JoinForm';
+import * as styles from './page.css';
+
 export default function JoinPage() {
-  return <p>JoinPage</p>;
+  return (
+    <div className={styles.container}>
+      <AuthHeader title="회원가입하기" description="Balenz에 오신 것을 환영합니다" />
+
+      <div className={styles.content}>
+        <JoinForm />
+        <AuthSwitchLink message="이미 계정이 있으신가요?" actionText="로그인하기" href="/login" />
+      </div>
+
+      <SocialLoginSection />
+    </div>
+  );
 }
