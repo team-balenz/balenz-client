67199182f80b945462c5d8188f3c01e5b09d881f
ynzung <syj91105@sookmyung.ac.kr>
2026-06-24 22:59:58 +0900

    refactor: 공유/스크랩 버튼과 토스트 로직 공통 컴포넌트로 분리(#183)

---
+++ src/shared/components/contentActionButton/constants.ts
@@ -0,0 +1,17 @@
+export const CONTENT_ACTION_ICON_PATH = {
+  share: '/icons/ic_share.svg',
+  bookmarkFilled: '/icons/ic_bookmark_filled.svg',
+  bookmarkOutlined: '/icons/ic_bookmark_outlined.svg',
+} as const;
+
+export const CONTENT_ACTION_TEXT = {
+  share: {
+    default: '공유하기',
+    mobile: '공유',
+  },
+  scrap: {
+    create: '스크랩하기',
+    cancel: '스크랩취소',
+    mobile: '스크랩',
+  },
+} as const;
