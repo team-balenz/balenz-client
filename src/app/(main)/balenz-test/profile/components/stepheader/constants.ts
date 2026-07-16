3859e356a02b274745b974b0d96fe2428d331fd3
ynzung <syj91105@sookmyung.ac.kr>
2026-07-16 23:01:50 +0900

    chore: StepHeader 문구 상수 추가(#192)

---
+++ src/app/(main)/balenz-test/profile/components/stepheader/constants.ts
@@ -0,0 +1,10 @@
+export const STEP_HEADER = {
+  BIRTH_YEAR: {
+    section: '응답자 정보',
+    title: '태어난 연도를 선택해 주세요.',
+  },
+  LEGAL_GENDER: {
+    section: '응답자 정보',
+    title: '법적 성별을 선택해 주세요.',
+  },
+} as const;
