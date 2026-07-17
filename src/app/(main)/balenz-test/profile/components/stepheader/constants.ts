389112883b720eac9594f6d24a1628b373df7b01
ynzung <syj91105@sookmyung.ac.kr>
2026-07-17 22:41:09 +0900

    refactor: StepHeader 폴더 삭제(#194)

--- src/app/(main)/balenz-test/profile/components/stepheader/constants.ts
+++
@@ -1,8 +0,0 @@
-import type { ProfileStepTypes } from '../../types/header';
-
-export const STEP_HEADER_LABEL = '응답자 정보';
-
-export const PROFILE_STEP_TITLE = {
-  birthYear: '태어난 연도를 선택해 주세요.',
-  gender: '법적 성별을 선택해 주세요.',
-} satisfies Record<ProfileStepTypes, string>;
